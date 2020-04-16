92ba598132c5ed1af4c21a7187742f5fab4e215a
EDMTDev <eddydn@gmail.com>
2020-04-16 09:58:49 +0700

    Migrate to AndroidX

--- app/src/main/java/dev/edmt/chatapp/MainActivity.java
+++ app/src/main/java/dev/edmt/chatapp/MainActivity.java
@@ -1,15 +1,14 @@
 package dev.edmt.chatapp;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -26,7 +25,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
 import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
-import hani.momanii.supernova_emoji_library.Helper.EmojiconTextView;
 
 public class MainActivity extends AppCompatActivity {
 
